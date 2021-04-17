8d70af7723ad03658c99d0679c5400caaf7083b0
bidyut.apece@gmail.com <bidyut.apece@gmail.com>
2021-04-17 17:03:57 -0400

    add empty boolean array marked for DFS

--- Graph/Graph.h
+++ Graph/Graph.h
@@ -4,12 +4,17 @@ This is the header file for the graph class.
 class Graph{
     private:
         int graphLen; // number of nodes in the graph
+        //array marked[] = {false * graphLen}; // an array to keep  track of nodes visited in DFS
+        //bool marked [graphLen] = {0} ;
         std::vector<int> graphNodes; // a vector containing all the nodes of the graph
         std::vector<int> edgesA; // a vector containing one side (A) of the edges among nodes
         std::vector<int> edgesB; // a vector containing the other side (B) of the edges among nodes
 
     public:
         //Graph(int size); // constructor of the class
+        //bool marked = {false * graphLen};
+        //bool marked [graphLen] = {false} ;
+        bool marked [] ;
         void addNode(int node);
         void addEdge(int nodeA, int nodeB);
         bool containsNode(int node); // check if the node is in the graph
@@ -17,8 +22,9 @@ class Graph{
         void deleteNode(int node);
         void deleteEdge(int nodA, int nodeB);
         void printGraph();
+        void printDFS(int node);
         void neighborsList(); // calcualtes neighbors for all nodes in the Graph
-        int nodeNeighbors(int node); // returs list of neighbors of a node
+        std::vector<int> nodeNeighbors(int node); // returs list of neighbors of a node
         int getGraphSize(); // returns total number of nodes in the graph
         Graph(int len); // constructor of the class
 
