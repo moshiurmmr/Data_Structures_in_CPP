672a45a6b7fa882969b9d38f3e7bcc8f5daf1c12
bidyut.apece@gmail.com <bidyut.apece@gmail.com>
2021-04-18 11:21:05 -0400

    remove DFS testing

--- Graph/main.cpp
+++ Graph/main.cpp
@@ -7,7 +7,7 @@
 #include "Graph.cpp" // if not added this line Linker error shows 'undefined referect to Graph::Graph(int)'
     // fix it. compiling both Graph.cpp and main.cpp from command prompt by "g++ Graph.cpp main.cpp" does not throw
     // any error
-
+//#include "bfs.cpp"
 int main(){
     int graph_size = 5;
     Graph mygraph(graph_size);
@@ -23,10 +23,21 @@ int main(){
     mygraph.containsNode(3);
     mygraph.containsNode(6);
     mygraph.neighborsList();
-    mygraph.deleteNode(4);
-    mygraph.printGraph();
-    mygraph.neighborsList();
-    mygraph.printGraph();
+    //mygraph.deleteNode(4);
+    //mygraph.printGraph();
+    //mygraph.neighborsList();
+    //mygraph.printGraph();
+    //std::cout << "marked" << mygraph.marked;
+    //std::cout << "print using DFS, nodeNeighbors(1) is " << std::endl;
+    //mygraph.printDFS(1);
+
+    //std::vector<int> neighbors = mygraph.nodeNeighbors(2);
+    //for(std::vector<int>::iterator i=neighbors.begin(); i != neighbors.end(); i++)
+       //std::cout << *i << " ";
+    //std::queue<int> bfs_queue; // an empty queue to track the neighbor nodes
+    //bool visited[mygraph.getGraphSize()]; // to track the nodes that have been visited
+    //std::cout << "print the graph using bfs" << std:endl;
+    //bfs(mygraph, 2);
 
     return 0;
 }
